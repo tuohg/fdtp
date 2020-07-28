c6ecd7687106a9015089bea1778a500e34bdc7a8
tuoh <tuoh@sheridancollege.ca>
2020-07-28 13:07:04 -0400

    update enum comments and add Unocard class

---
+++ src/ca/sheridancollege/project/entity/UnoCard.java
@@ -0,0 +1,30 @@
+package ca.sheridancollege.project.entity;
+
+import ca.sheridancollege.project.Card;
+
+/**
+ *
+ * This class defines a specific Uno card.
+ * @author Hangge Tuo
+ * Created: Jul 28, 2020
+ * Updated: Jul 28, 2020
+ */
+public class UnoCard extends Card{
+    private UnoCardColor cardColor;
+    private UnoCardValue cardValue;
+    private UnoCardState cardState;
+
+    public UnoCard(UnoCardColor cardColor, UnoCardValue cardValue, UnoCardState cardState) {
+        this.cardColor = cardColor;
+        this.cardValue = cardValue;
+        this.cardState = cardState;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return this.cardColor+"-"+this.cardValue;
+    }
+
+}
--- src/ca/sheridancollege/project/entity/UnoCardValue.java
+++ src/ca/sheridancollege/project/entity/UnoCardValue.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.sheridancollege.project.entity;
 
 /**
- *
- * @author tuohg
+ * The enum defines the values of Uno card game
+ * @author Hangge Tuo
+ * Created: Jun 16, 2020
+ * Updated: Jul 28, 2020
  */
 public enum UnoCardValue {
     ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,REVERSE,SKIP,DRAWTWO,DRAWFOUR,WILD
