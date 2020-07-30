d5e256ba2368e4de89d9bacdf27dcf2e4444c7e0
tuoh <tuoh@sheridancollege.ca>
2020-07-30 12:04:38 -0400

    modify UnoDeck class: override shuffle method and add showCards method

--- src/ca/sheridancollege/project/entity/UnoDeck.java
+++ src/ca/sheridancollege/project/entity/UnoDeck.java
@@ -1,38 +1,47 @@
 package ca.sheridancollege.project.entity;
 
 import ca.sheridancollege.project.Card;
+import ca.sheridancollege.project.GroupOfCards;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * This class +++Insert Description Here+++
  * @author Hangge Tuo
  * Created: Jun 28, 2020
- * Updated: Jul 28, 2020
+ * Updated: Jul 30, 2020
  */
 
-public class UnoDeck {
-    private UnoCard[] cards;
+public class UnoDeck extends GroupOfCards{
+    private UnoCard[] unoCards;
     private ArrayList<Card> availablePile;
     private ArrayList<Card> discardPile;
     
     //main class for test
     public static void main(String[] args) {
-        UnoDeck u1 = new UnoDeck();
+        UnoDeck u1 = new UnoDeck(108);
         u1.initDeck();
-        int count =0;
-        for (int i = 0; i < u1.cards.length; i++) {
-            System.out.println(++count + " " +u1.cards[i].toString());
-        }
+        System.out.println("Before shuffle:");
+        u1.showCards(u1.availablePile);
+        System.out.println("After shuffle:");
+        u1.shuffle();
+        u1.showCards(u1.availablePile);
     }
     
     /**
      * UnoDeck constructor
      */
-    public UnoDeck() {
-        cards = new UnoCard[108];
+    public UnoDeck(int size) {
+        super(size);
+        unoCards = new UnoCard[108];
     }
     
+    /**
+     * The method is for initialing the game cards, build 108 cards and add to availablePile
+     */
     public void initDeck() {
         UnoCardColor[] colors = UnoCardColor.values();
         UnoCardValue[] values = UnoCardValue.values();
@@ -43,9 +52,9 @@ public class UnoDeck {
             UnoCardColor color = colors[i];
             for (int j = 0; j < 13; j++) {
                 UnoCardValue value = values[j];
-                cards[cardsInDeck++] = new UnoCard(color, value);
+                unoCards[cardsInDeck++] = new UnoCard(color, value);
                 if (j>0) {
-                    cards[cardsInDeck++] = new UnoCard(color, value);
+                    unoCards[cardsInDeck++] = new UnoCard(color, value);
                 }
             }
         }
@@ -54,14 +63,42 @@ public class UnoDeck {
         UnoCardValue[] values1 = new UnoCardValue[]{UnoCardValue.WILD, UnoCardValue.DRAWFOUR};
         for (UnoCardValue value : values1) {
             for (int i = 0; i < 4; i++) {
-                cards[cardsInDeck++] = new UnoCard(UnoCardColor.WILD, value);
+                unoCards[cardsInDeck++] = new UnoCard(UnoCardColor.WILD, value);
             }
         }
         //Add whole Uno cards to availablePile.
-        for (UnoCard card : cards) {
+        availablePile = new ArrayList<Card>();
+        discardPile = new ArrayList<Card>();
+        for (UnoCard card : unoCards) {
             availablePile.add(card);
         }
         //Empty dicardPile
         discardPile.clear();
     }
+
+    /**
+     * Override shuffle method of parent class to add condition for starting game.
+     */
+    @Override
+    public void shuffle() {
+        if (availablePile.size() != 108) {
+            try {
+                throw new Exception("The number of cards should be 108.");
+            } catch (Exception ex) {
+                Logger.getLogger(UnoDeck.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        Collections.shuffle(availablePile);
+    }
+    /**
+     * The method is for displaying all cards as strings
+     * @param cards The collection of CARDS that need to be displayed
+     */
+    private void showCards(ArrayList<Card> cards){
+        int count=0;
+        for (int i = 0; i < cards.size(); i++) {
+            System.out.println(++count + " " +cards.get(i).toString());
+        }
+    }
+    
 }
