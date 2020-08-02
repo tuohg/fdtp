3a64d3242009a11acbd752465d01601778d3922c
gracejpark <parkyee@sheridancollege.ca>
2020-08-02 23:54:13 +0900

    Narrtive Usecase 1-8. (game.init)

--- src/ca/sheridancollege/project/controller/UnoGamePlay.java
+++ src/ca/sheridancollege/project/controller/UnoGamePlay.java
@@ -2,23 +2,28 @@ package ca.sheridancollege.project.controller;
 import ca.sheridancollege.project.Card;
 import ca.sheridancollege.project.Player;
 import ca.sheridancollege.project.entity.*;
-import java.util.ArrayList;
+import java.util.*;
+
 /**
  * @author Ye Eun Park is going to be the greatest Java programmer of all time
  */
 public class UnoGamePlay extends Game
 {
-   private ArrayList<Player> currentPlayers;
+   //private ArrayList<Player> currentPlayers;
    //private UnoDeck deck;
    //private ArrayList<Card> playerHand;
    private boolean gameDirection; // true==clockwise, false==couterclockwise
    private Player currentPlayer;
    private int currentPlayerID;
+   private UnoCard topCard;
+
    private UnoCardColor validColor;
    private UnoCardValue validValue;
    private static final UnoCardColor WILD = UnoCardColor.WILD;
 
    public table  = UnoTable.getInstance();
+   private pM  = PlayersManager.getInstance();
+   private int totalPlayer = pM.totalPlayer;
 
    public UnoGamePlay (String name)
    {
@@ -26,21 +31,67 @@ public class UnoGamePlay extends Game
       currentPlayers = new ArrayList<Player>();
    }
 
-   //public Game -> chk
-   public void start ()
+   public void init ()
    {
+      //1,2
       table.prepareTable();
-      validColor = table.showTopCard().getCardColor();
-      validValue = table.showTopCard().getCardValue();
-      gameDirection = true;
-      //currentPlayer = 
-      play();
+      SignUpPlayers.sign();
+      //3
+
+      //4
+      int max = 0;
+      for (Player player : pM.currentPlayers) {
+         UnoCard card = table.pullCard();
+         UnoCardValue cardValue = card.getCardValue();
+         int value = cardValue.ordinal() > 9 ? 0 : cardValue.ordinal();
+         if (value > max) {
+            currentPlayer = player; //currentPlayer == dealer
+         }
+         //5 
+         table.pushCard(card);
+      }
+
+      //6
+      for (int i = 0; i < pM.totalPlayer; i++) {
+         int index = (i + currentPlayer.id) % totalPlayer;
+         for (int j = 0; j < 7; j++) {
+            pM.currentPlayers[index].getCard(table.pullCard());
+         }
+      }
+
+      //7
+      topCard = table.pullCard();
+      //8
+      currentPlayer = pM.currentPlayers[(currentPlayer.id++) % totalPlayer];
+   }
+
+   @Override
+   public void play ()
+   {
+      while (true) {
+         //valid color
+
+         //valid value(0-9)
+
+         //wildcard
+
+         //9-1
+
+
+         //9-2
+         //player submit wild card
+      }
+   }
+
+   @Override
+   public void declareWinner ()
+   {
+      throw new UnsupportedOperationException("Not supported yet.");
    }
 
    public UnoCard getTopCard ()
    {
-      Unocard tmp;
-      return tmp;
+      return table.pullCard();
    }
 
    public Player getPreviousPlayer (int number)
@@ -105,10 +156,10 @@ public class UnoGamePlay extends Game
    public void setNextPlayer (int number)
    {
       if (gameDirection) {
-         currentPlayer = currentPlayers[(currentPlayerID + number) % 4];
+         currentPlayer = pM.currentPlayers[(currentPlayerID + number) % totalPlayer];
       }
       else {
-         currentPlayer = currentPlayers[(currentPlayerID - number + 4) % 4];
+         currentPlayer = pM.currentPlayers[(currentPlayerID - number + totalPlayer) % totalPlayer];
       }
    }
 
@@ -117,39 +168,11 @@ public class UnoGamePlay extends Game
       validColor = card.cardColor;
    }
 
-   //TODO: check below 2 functions
-   public void submitDraws (String id)
-   {
-   }
-
-   public void submitPlayerCard ()
-   {
-   }
-
-   public void sayUno ()
-   {
-   }
-
    public boolean askReplay ()
    {
-   }
-
-   @Override
-   public void play ()
-   {
-      while (true) {
-         currentPlayer
-      }
-   }
-
-   @Override
-   public void declareWinner ()
-   {
-      throw new UnsupportedOperationException("Not supported yet.");
-   }
-
-//give player cards
-   public void giveCards (Card topCard)
-   {
+      Scanner src = new Scanner(System.in);
+      String output = src.nextLine();
+      System.out.println("Replay? y/n");
+      return "y".equals(output);
    }
 }
