12a34eb45c6f125924148c7a0a0f16e339f1eb3f
LiyaDing <dingliy@sheridancollege.ca>
2020-08-09 04:38:39 -0400

    Add files via upload

--- src/ca/sheridancollege/project/player/UnoPlayer.java
+++ src/ca/sheridancollege/project/player/UnoPlayer.java
@@ -1,103 +1,115 @@
 package ca.sheridancollege.project.player;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-import ca.sheridancollege.project.Player;
 
+import ca.sheridancollege.project.Player;
+import ca.sheridancollege.project.UnoCard;
 /**
  * This is a subclass of Player.java
  *
  * @author Liya
  */
 public class UnoPlayer extends Player {
+	private int playerId;
+	private Hand p_hand;
+	private String p_name;
 
-    ArrayList<String> playerList = new ArrayList<>();
-    protected int winTimes = 0;
-    protected int loseTimes = 0;
-    private boolean isDealer = false;
-
-    public UnoPlayer(String name) {
-        super(name);
-    }
-
+	public UnoPlayer(String name, int playerId) {
+		this.playerId   = playerId;
+		this.p_hand = new Hand();
+		this.p_name = name;
+	}        
     @Override
     public void play() {
-
-        //case 1:player name
-        //case 1.1: the first player come in
-
-        while (playerList.size() < 4) {
-
-            if (!isPlayerExist(this.getName())) {
-                System.out.println("successfully!  Your playername can be used.");
-                playerList.add(this.getName());
-
-            } else {
-                System.out.println("Please enter your name as the unique ID: ");
-                Scanner sc = new Scanner(System.in);
-                this.setName(sc.nextLine());
-            }
-        }
-        if (playerList.size() >= 4) {
-            System.out.println("Now the players are " + playerList.size());
-        }
-
-        //show the playerList        
-        System.out.println("Players names are: ");
-        for (int i = 0; i < playerList.size(); i++) {
-            System.out.print(playerList.get(i) + "\t");
-        }
-        System.out.println();
-
-        System.out.println("Welcome to UNO game! Start now(Y/N)?");
-        Scanner s = new Scanner(System.in);
-        if (s.next().equalsIgnoreCase("Y")) {
-
-            Random random = new Random();
-            int r = random.nextInt(playerList.size());
-
-            System.out.println(playerList.get(r) + " is the Dealer");
-            
-        }
+      
     }
-
-    /**
-     *
-     * @param name
-     * @return
-     */
-    public boolean isPlayerExist(String name) {
-        boolean isExist = true;
-        if (playerList.lastIndexOf(name) == -1) {
-//         System.out.println("Your player name can be used.");// Returning -1 means that no same name exist
-            isExist = false;
-        }
-        return isExist; // return true means the name already exist
-    }
-
-    public int getWinTimes() {
-        return winTimes;
-    }
-
-    public void setWinTimes(int winTimes) {
-        this.winTimes = winTimes;
-    }
-
-    public int getLoseTimes() {
-        return loseTimes;
-    }
-
-    public void setLoseTimes(int loseTimes) {
-        this.loseTimes = loseTimes;
-    }
-
-    public boolean isIsDealer() {
-        return isDealer;
-    }
-
-    public void setIsDealer(boolean isDealer) {
-        this.isDealer = isDealer;
-    }
-
+	/**
+	 * Get the Identification (ID) of the player.
+	 * @return the player's ID.
+	 */
+	public int getPlayerId(){
+		return this.playerId;
+	}
+	
+	/**
+	 * Get the name of the player.
+	 * @return the player's name.
+	 */
+	public String getName(){
+		return this.p_name;
+	}
+	
+	/**
+	 * @return the number of cards present in hand
+	 */
+	public int numCards(){
+		return p_hand.getNumCards();
+	}
+	
+	/**
+	 * The player takes a card if it's its turn.
+	 * @param card that will be taken.
+	 */
+	public boolean takeCard(UnoCard card){
+		return this.p_hand.addCard(card);
+	}
+	
+	/**
+	 * Play a card if it is its turn.
+	 * @param cardName: The name of the card. The name is compose by the color 
+	 * and the number or effect.
+	 * @return the card.
+	 */
+	public UnoCard playCard(String cardName){
+		int i = 0;
+		while(i != this.p_hand.getNumCards()){
+			if(this.p_hand.showCard(i).equals(cardName))
+				return this.p_hand.getCard(i);
+			i++;
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Play the ith card in the hand of the player; 
+	 * @param i index of the card.
+	 * @return the ith card or null if there is no ith card.
+	 */
+	public Card playCard(int i){
+		return this.p_hand.getCard(i);
+	}
+	
+	/**
+	 * Print all the cards on the hand of the player.
+	 */
+	public void showCards(){
+		int ncards =  this.p_hand.getNumCards();
+		String toPrint = "";
+		int i;
+		if(ncards == 0){
+			System.out.print("EMPTY HAND.");
+			
+		} else{
+			for(i = 0; i < ncards; i++){
+				toPrint += "[" + this.p_hand.showCard(i) + "] ";
+			}
+		}
+		
+		if(toPrint.length() > 80){
+			for(i = 0; i < toPrint.length()/80; i++)
+				System.out.println(toPrint.substring(i*80, i*80 + 80));
+			System.out.println(toPrint.substring(i*80));
+		} else {
+			System.out.println(toPrint);
+		}
+	}
+	
+	/**
+	 * Empty the hand.
+	 */
+	public void emptyHand(){
+		while(!this.p_hand.isEmpty())
+			this.p_hand.getCard(0);
+		}
+    
 }
---
+++ src/ca/sheridancollege/project/player/UnoPlayerManager.java
@@ -0,0 +1,171 @@
+
+package ca.sheridancollege.project.player;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Liya
+ */
+public class UnoPlayerManager {
+	private ArrayList <UnoPlayer> players;
+	private boolean canAdd;
+	private int currElem;
+	private int nextElem;
+	private boolean toRight;
+	private static UnoPlayerManager pM = null;
+	
+	private UnoPlayerManager(){
+		this.players = new ArrayList<UnoPlayer>();
+		this.canAdd  = true;
+		this.currElem = 0;
+		this.nextElem = 0;
+		this.toRight = true;
+	}
+	
+	public static UnoPlayerManager getInstance(){
+		if(pM == null)
+			pM = new UnoPlayerManager();
+		
+		return pM;			
+	}
+	
+	/**
+	 * Add a player in the list only if inclusion of new players is 
+	 * allowed.
+	 * @param player to be add.
+	 * @return true if it was possible to add the player, false otherwise.
+	 */
+	public boolean addPlayer(UnoPlayer player){
+		if(this.canAdd){
+			this.players.add(player);
+			
+			if(this.players.size() == 2)
+				this.nextElem = 1;
+		}
+			
+		return this.canAdd;
+	}
+	
+	/**
+	 * Return the status of the players. This status is a format string
+	 * composed by many "PlayerName : NumberOfCards".
+	 * @return a string that contains the status of all the cards.
+	 */
+	public String getPlayersStatus(){
+		String pStatus = "";
+		UnoPlayer player;
+		for(int i = 0; i < this.players.size(); i++){
+			player  = this.players.get(i);
+			pStatus += "[" +player.getName() + ": ";
+			pStatus += Integer.toString(player.numCards()) + "] ";
+		} 
+		
+		return pStatus;
+	}
+	
+	/**
+	 * Get the number of players in the list.
+	 * @return the number of players.
+	 */
+	public int getNumPlayers(){
+		return this.players.size();
+	}
+	
+	/**
+	 * Signal to stop allowing new players.
+	 */
+	public void lockAdd(){
+		this.canAdd = false;
+	}
+	
+	/**
+	 * Signal to allow new players.
+	 */
+	public void unlockAdd(){
+		this.canAdd = true;
+	}
+	
+	/**
+	 * Get the direction of the rotation of the players.
+	 * @return "RIGHT"(increasing) or "LEFT"(decreasing).
+	 */
+	public String getDirection(){
+		if(this.toRight)
+			return "RIGHT";
+		else
+			return "LEFT";
+	}
+	
+	/** 
+	 * Change the direction of the rotation.
+	 */
+	public void changeDirection(){
+		this.toRight ^= true;
+		
+		if(this.toRight)
+			this.nextElem = (this.currElem + 1) % players.size();
+		else
+			this.nextElem = (this.currElem - 1 + players.size()) % players.size();
+	}
+	
+	
+	/**
+	 * Rotate the players.
+	 */
+	public void rotate(){	
+		//this.players.get(this.currElem).passTurn(this.players.get(this.nextElem));
+		this.currElem = this.nextElem;
+		this.rotateNextPlayer();
+	}
+	
+	/**
+	 * Rotate only the next player.
+	 */
+	public void rotateNextPlayer(){
+		
+		if(this.toRight)
+			this.nextElem = (this.nextElem + 1) % players.size();
+		else			
+			this.nextElem = (this.nextElem - 1 + players.size()) % players.size();
+	}
+	
+	/**
+	 * Get the current player.
+	 * @return the current player.
+	 */
+	public UnoPlayer getCurrent(){
+		UnoPlayer p;
+		try{
+			p = this.players.get(this.currElem);
+			
+		} catch(IndexOutOfBoundsException e){
+			System.out.println("There isn't any players.");
+			p = null;
+		}
+			
+		return p; 
+	}
+	
+	/**
+	 * Get the next player.
+	 * @return the next player.
+	 */
+	public UnoPlayer getNext(){
+		return this.players.get(this.nextElem);
+	}
+	
+	/**
+	 * Look for a player that have no more cards in the hand.
+	 * @return the winner, if there is one.
+	 */
+	public UnoPlayer lookForWinner(){
+		for(int i = 0; i < this.players.size(); i++){
+			if(this.players.get(i).numCards == 0)
+				return this.players.get(i);
+		}
+		
+		return null;
+	}
+}
+
