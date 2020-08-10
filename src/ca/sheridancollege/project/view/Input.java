fbed7461a5661f06ef39069ad7e317cb1238a2a2
tuoh <tuoh@sheridancollege.ca>
2020-08-09 22:42:45 -0400

    Update Input class

--- src/ca/sheridancollege/project/view/Input.java
+++ src/ca/sheridancollege/project/view/Input.java
@@ -1,27 +1,80 @@
 package ca.sheridancollege.project.view;
-import java.io.InputStream;
+
 import java.util.Scanner;
 
 /**
- * This class read user/player input from the keyboard
- *  the input is divided into set of fields, which is return as array of strings
- * 
+ * This class read user/player input from the keyboard the input is divided into set of fields, which is return as array
+ * of strings
+ *
  * @author Hanan Fadel July 2020
- */ 
+ */
 public class Input {
 
-	 private Scanner input = null;
-	 
-	 public Input(InputStream in){
-	 	input = new Scanner(in);
-	 }
-	 	
-	 public String[] readFields(){
-	 	return this.input.nextLine().trim().split("\\s+");
-	 }
-	 	
-	 public void close(){
-	 	if(null != input)
-	 		this.input.close();
-	 }
+    public static int getInt(String prompt, int start, int end) {
+        while (true) {
+            System.out.println(prompt);
+            try {
+                Scanner scan = new Scanner(System.in);
+                int input = scan.nextInt();
+                if (input >= start && input <= end) {
+                    return input;
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                if (start > end) {
+                    System.out.println("The argument 'start' should less than 'end', please re-enter:");
+                } else {
+                    System.out.println("Please enter the number between " + start + " and " + end);
+
+                }
+            }
+        }
+    }
+
+    public static String getString(String prompt, int start, int end) {
+        while (true) {
+            System.out.println(prompt);
+            try {
+                Scanner scan = new Scanner(System.in);
+                String input = scan.next();
+                if (input.length() >= start && input.length() <= end) {
+                    return input;
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                if (start == end) {
+                    System.out.println("Please enter a string of length " + start);
+                } else if (start > end) {
+                    System.out.println("The argument 'start' should less than 'end', please re-enter:");
+                } else {
+                    System.out.println("Please enter the string between " + start + " and " + end);
+                }
+            }
+        }
+    }
+
+    public static String getString(String prompt, int length) {
+        return getString(prompt, length, length);
+    }
+
+    public static String getString(String prompt, String[] strArray) {
+        String strReturn = getString(prompt, 1, 100);
+        String strAllChoice = "";
+        for (int i = 0; i < strArray.length; i++) {
+            strAllChoice = strArray[i] + "/";
+        }
+        strAllChoice += "\b\n";
+        while (true) {
+            for (int i = 0; i < strArray.length; i++) {
+                if (strArray[i].equals(strReturn)) {
+                    return strReturn;
+                }
+            }
+            System.out.println("Input error, please re-enter all choices below: ");
+            System.out.println("Choices: "+strAllChoice);
+        }
+    }
+
 }
